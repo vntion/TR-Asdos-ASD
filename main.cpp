1b4dc43c14db0cf5e85264c7ee0b4c5e36e0e64d
Alifio431 <alifio431@gmail.com>
2024-06-26 08:45:59 +0700

    p

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+struct pharmacist{
+    int no_id;
+    string nama_apoteker;
+};
+struct medicine{
+    string nama_obat;
+    int kode_obat;
+    string expired_obat;
+    string supplier;
+};
+struct buyer{
+    string nomor_bpjs;
+    string nama_buyer;
+};
+struct receipts{
+    string tanggal;
+    string nomor_bpjs;
+    string nama_buyer;
+    string nama_obat;
+    int quantity;
+    long harga_total;
+};
+
+int main(){
+
+    return 0;
+}
