6cbee87025307c547ed03b3b6cc6e2672de941ef
Baskoro <139613178+vntion@users.noreply.github.com>
2024-07-03 14:14:58 +0700

    update

--- main2.cpp
+++ main2.cpp
@@ -13,6 +13,7 @@ const string nama_file = "data2.txt";
 // Linked list
 struct Data{
     int no_id;
+    int no_id_apoteker;
     string nama_apoteker;
     string tanggal;
     string nomor_bpjs;
@@ -41,26 +42,27 @@ void get_data(){
     string line;
 
     while (getline(inFile, line)) {
-        string arr[11];
+        string arr[12];
         int i = 0;
         stringstream ssin(line);
-        while (ssin.good() && i < 11) {
+        while (ssin.good() && i < 12) {
             ssin >> arr[i];
             i++;
         }
 
         Data* newData = new Data;
         newData -> no_id = stoi(arr[0]);
-        newData -> nama_apoteker = arr[1];
-        newData -> tanggal = arr[2];
-        newData->nomor_bpjs = arr[3];
-        newData->nama_buyer = arr[4];
-        newData->nama_obat = arr[5];
-        newData->kode_obat = stoi(arr[6]);
-        newData->expired_obat = arr[7];
-        newData->supplier = arr[8];
-        newData->jumlah = stoi(arr[9]);
-        newData->harga_total = stol(arr[10]);
+        newData->no_id_apoteker = stoi(arr[1]);
+        newData -> nama_apoteker = arr[2];
+        newData -> tanggal = arr[3];
+        newData->nomor_bpjs = arr[4];
+        newData->nama_buyer = arr[5];
+        newData->nama_obat = arr[6];
+        newData->kode_obat = stoi(arr[8]);
+        newData->expired_obat = arr[9];
+        newData->supplier = arr[10];
+        newData->jumlah = stoi(arr[11]);
+        newData->harga_total = stol(arr[12]);
         newData->next = NULL;
 
         if (HEAD == NULL) HEAD = newData;
@@ -85,7 +87,7 @@ void set_data() {
   Data* temp = HEAD;
 
   while (temp != NULL) {
-    outFile << temp -> no_id << " " << temp -> nama_apoteker << " " << temp -> tanggal << " " << temp -> nomor_bpjs << " " << temp -> nama_buyer << " " << temp -> nama_obat << " " <<
+    outFile << temp -> no_id << " " << temp->no_id_apoteker << " " <<temp -> nama_apoteker << " " << temp -> tanggal << " " << temp -> nomor_bpjs << " " << temp -> nama_buyer << " " << temp -> nama_obat << " " <<
     temp -> kode_obat << " " << temp -> expired_obat << " " << temp -> supplier << " " << temp -> jumlah << " " << temp -> harga_total << endl;
     temp = temp -> next;
   }
@@ -95,7 +97,7 @@ void set_data() {
 
 void tambah_data(){
     int input_jumlah_data; // Jumlah data yang ingin dimasukkan
-    int input_no_id, input_kode_obat, input_jumlah;
+    int input_no_id, input_kode_obat, input_jumlah, input_no_id_pegawai;
     long input_harga_total;
     string input_nama_apoteker, input_nama_obat, input_expired_obat, input_supplier, input_nomor_bpjs, input_nama_buyer, input_tanggal, nama_obat;
 
@@ -105,9 +107,18 @@ void tambah_data(){
 
     int i = 1; // Iteration
 
-    // User memasukkan input data 
+    // User memasukkan input data
     do {
         cout << "Data Apotik ke - " << i << endl << endl;
+
+        cout << "No id\t : ";
+        cin >> input_no_id;
+
+        cout << "No id apoteker\t : ";
+        cin >> input_no_id_pegawai;
+
+        cout << "Nama Apoteker\t : ";
+        cin >> input_nama_apoteker;
 
         cout << "Nama Pasien\t : ";
         cin >> input_nama_buyer;
@@ -141,6 +152,9 @@ void tambah_data(){
 
     // Setelah user memasukkan input, proses selanjutnya memasukkan input ke data
     Data* newData = new Data;
+    newData->no_id = input_no_id;
+     newData->no_id_apoteker = input_no_id_pegawai;
+    newData->nama_apoteker = input_nama_apoteker;
     newData->tanggal = input_tanggal;
     newData->nomor_bpjs = input_nomor_bpjs;
     newData->nama_buyer = input_nama_buyer;
@@ -159,7 +173,7 @@ void tambah_data(){
         temp->next = newData;
     }
 
-    // Menambahkan data ke file 
+    // Menambahkan data ke file
     set_data();
 }
 
@@ -181,6 +195,7 @@ void lihat_data(){
     while (temp != NULL) {
         cout << "Data ke - " << i << endl;
         cout << "No id :" << temp -> no_id << endl;
+        cout << "NO id pegawai :" << temp->no_id_apoteker << endl;
         cout << "Nama Apoteker :" << temp -> nama_apoteker << endl;
         cout << "Tanggal :" << temp -> tanggal << endl;
         cout << "No BPJS :" << temp -> nomor_bpjs << endl;
@@ -210,8 +225,27 @@ void hapus_data(){
     cout << "=====================================================================" << endl;
 
     // Render tabel data
+    Data* temp = HEAD;
+    int i = 1;
 
-    // Pencarian ID 
+    while (temp != NULL) {
+        cout << "Data ke - " << i << endl;
+        cout << "No id :" << temp -> no_id << endl;
+        cout << "NO id pegawai :" << temp->no_id_apoteker << endl;
+        cout << "Nama Apoteker :" << temp -> nama_apoteker << endl;
+        cout << "Tanggal :" << temp -> tanggal << endl;
+        cout << "No BPJS :" << temp -> nomor_bpjs << endl;
+        cout << "Nama Buyer :" << temp -> nama_buyer << endl;
+        cout << "Nama Obat :" << temp -> nama_obat << endl;
+        cout << "Kode Obat :" << temp -> kode_obat << endl;
+        cout << "Expired Obat :" << temp -> expired_obat << endl;
+        cout << "Supplier :" << temp -> supplier << endl;
+        cout << "Jumlah :" << temp -> jumlah << endl;
+        cout << "Harga Total :" << temp -> harga_total << endl << endl;
+        temp = temp -> next;
+    }
+
+    // Pencarian ID
     Data* search = HEAD;
     int cari_id;
     bool found = false;
@@ -236,7 +270,7 @@ void hapus_data(){
     }
 
     ///// Proses hapus data berdasarkan id
-    Data* temp = HEAD;
+    Data* cur = HEAD;
     Data* prev = NULL;
 
     // Jika id yang dicari berada di awal linked list
@@ -247,12 +281,12 @@ void hapus_data(){
     }
 
     // Jika id yang dicari tidak di awal linked list
-    while (temp != NULL && temp->no_id != cari_id) {
-        prev = temp;
-        temp = temp->next;
+    while (cur != NULL && cur->no_id != cari_id) {
+        prev = cur;
+        cur = cur->next;
     }
 
-    prev->next = temp->next;
+    prev->next = cur->next;
 
     // Menambahkan data ke file
     set_data();
@@ -293,6 +327,7 @@ void edit_data(){
     }
 
     int no_id;
+     int no_id_apoteker;
     string nama_apoteker;
     string tanggal;
     string nomor_bpjs;
@@ -387,71 +422,131 @@ void edit_data(){
     set_data();
 }
 
-// void cari_data(){
-//     int choice;
-//     do{
-//         cout << "Cari berdasarkan apa : "     << endl;
-//         cout << "1. NO ID \t"         << "5. NAMA BUYER \t"     << "9. SUPPLIER \t"     << endl;
-//         cout << "2. NAMA APOTEKER \t" << "6. NAMA OBAT \t"      << "10. JUMLAH \t"      << endl;
-//         cout << "3. TANGGAL \t"       << "7. KODE OBAT \t"      << "11. HARGA TOTAL \t" << endl;
-//         cout << "4. NO BPJS \t"       << "8. EXPIRED OBAT \t"   << "12. KEMBALI \t"     << endl;
+void cari_IDapoteker(){
+    int search_id;
 
-//         switch (choice) {
-//                         case 1:
-//                             cari_id();
-//                             break;
-//                         case 2:
-//                             cari_apoteker();
-//                             break;
-//                         case 3:
-//                             cari_tanggal();
-//                             break;
-//                         case 4:
-//                             cari_bpjs();
-//                             break;
-//                         case 5:
-//                             cari_namabuyer();
-//                             break;
-//                         case 6:
-//                             cari_namaobat();
-//                             break;
-//                         case 7:
-//                             cari_kodeobat();
-//                             break;
-//                         case 8:
-//                             cari_expobat();
-//                             break;
-//                         case 9:
-//                             cari_supplier();
-//                             break;
-//                         case 10:
-//                             cari_jumlahobat();
-//                             break;
-//                         case 11:
-//                             cari_hargatotal();
-//                             break;
-//                         case 12:
-//                             cout << "Keluar";
-//                             getchar();
-//                             getchar();
-//                             break;
-//                         default:
-//                             cout << "Pilihan tidak valid.";
-//                             getchar();
-//                             getchar();
-//                             break;
+    cout << "Masukkan keyword id yang ingin dicari : ";
+    cin >> search_id;
 
-//                 } while (choice != 12);
-//     }while(choice !=12);
-    
-// }
+    Data* temp = HEAD;
+    while (temp != NULL) {
+        if(search_id!=temp->no_id){
+            temp = temp->next;
+        }
+        else{
+            cout << "Data ditemukan !" << endl;
+            cout << temp->no_id << endl;
+            cout << temp->nama_apoteker << endl;
+            break;
+        }
+    }
+}
+
+void cari_data(){
+    int choice;
+
+
+    do{
+        cout << "Cari berdasarkan apa : "     << endl;
+        cout << "1. NO ID \t"         << "5. NAMA BUYER \t"     << "9. SUPPLIER \t"     << endl;
+        cout << "2. NAMA APOTEKER \t" << "6. NAMA OBAT \t"      << "10. JUMLAH \t"      << endl;
+        cout << "3. TANGGAL \t"       << "7. KODE OBAT \t"      << "11. HARGA TOTAL \t" << endl;
+        cout << "4. NO BPJS \t"       << "8. EXPIRED OBAT \t"   << "12. KEMBALI \t"     << endl;
+
+        cin >> choice;
+        switch (choice) {
+                        case 1:
+                            cari_IDapoteker();
+                            break;
+                        // case 2:
+                        //     cari_apoteker();
+                        //     break;
+                        // case 3:
+                        //     cari_tanggal();
+                        //     break;
+                        // case 4:
+                        //     cari_bpjs();
+                        //     break;
+                        // case 5:
+                        //     cari_namabuyer();
+                        //     break;
+                        // case 6:
+                        //     cari_namaobat();
+                        //     break;
+                        // case 7:
+                        //     cari_kodeobat();
+                        //     break;
+                        // case 8:
+                        //     cari_expobat();
+                        //     break;
+                        // case 9:
+                        //     cari_supplier();
+                        //     break;
+                        // case 10:
+                        //     cari_jumlahobat();
+                        //     break;
+                        // case 11:
+                        //     cari_hargatotal();
+                        //     break;
+                        // case 12:
+                        //     cout << "Keluar";
+                        //     getch();
+                        //     break;
+                        default:
+                            cout << "Pilihan tidak valid.";
+                            getch();
+                            break;
+
+                } while (choice != 12);
+    }while(choice !=12);
+
+}
 
 void urutkan_data(){
 
 }
 
+void menuUtama(){
+    int input;
+
+    while (true) {
+        system("cls");
+        cout << "1. Tambah data \t" << endl;
+        cout << "2. Lihat data \t" << endl;
+        cout << "3. Hapus data \t" << endl;
+        cout << "4. Edit data \t" << endl;
+        cout << "5. Cari data \t" << endl;
+        cout << "6. Urutkan data \t" << endl;
+        cout << "7. Keluar \t" << endl;
+        cin >> input;
+
+        if (input == 1) {
+            tambah_data();
+        }
+        if (input == 2) {
+            lihat_data();
+        }
+        if (input == 3) {
+            hapus_data();
+        }
+        if (input == 4) {
+            edit_data();
+        }
+         if (input == 5) {
+            cari_data();
+        }
+         if (input == 6) {
+            edit_data();
+        }
+        if (input == 7) {
+            break;
+        }
+    }
+}
+
 int main(){
     get_data(); // Mengambil data dari file txt
+    menuUtama();
 
     return 0;
 }
